da282fd549ca537c9bae785747b09e117d1b9314
RodrigofirminoA <132634270+RodrigofirminoA@users.noreply.github.com>
2025-11-17 21:14:39 -0300

    Add files via upload

--- topo.php
+++ topo.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Final</title>
 </head>
+
 <body>
     <nav>
         <div class="menu">
             <ul>
-                <li class="nav-iten">Home</li>
-                <li class="nav-iten">Portfólio</li>
-                <li class="nav-iten">Pacotes</li>
-                <li class="nav-iten">Redes Sociais</li>
-                <li class="nav-iten">Reserva</li>
+                <li class="nav-iten">
+                    <a>Home</a>
+                </li>
+                <li class="nav-iten">
+                    <a href="">Portfólio</a>
+                </li>
+                <li class="nav-iten">
+                    <a href="">Pacotes</a>
+                </li>
+                <li class="nav-iten">
+                    <a href="">Redes Sociais</a>
+                </li>
+                <li class="nav-iten">
+                    <a href="">Reserva</a>
+                </li>
             </ul>
         </div>
     </nav>
