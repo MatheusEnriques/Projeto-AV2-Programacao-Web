2b8b84f4ecd401033bab692c61565b3811663162
RodrigofirminoA <132634270+RodrigofirminoA@users.noreply.github.com>
2025-11-24 20:12:41 -0300

    Add files via upload

---
+++ reserva.php
@@ -0,0 +1,18 @@
+<form action="dados.php" method="$_POST">
+<label for="nome">Nome:</label>
+<input type="text" name="cliente" placeholder="Digite seu Nome"><br>
+
+<label for="email">Email:</label>
+<input type="text" name="email" placeholder="Digite seu Email"><br>
+
+<label for="telefone">Telefone:</label>
+<input type="tel" name="telefone"><br>
+
+<label for="data">Data</label>
+<input type="date" placeholder="Data Sugerida" name="data"><br>
+
+<label for="hora">Horas:</label>
+<input type="time" name="hora"><br>
+
+<button type="submit" name="">Enviar</button>
+</form>
---
+++ topo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Final</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <span class="logotipo"><img src="./logo.JPG" alt="logo"></span>
+    <nav>
+        
+            <ul class="nav-links">
+                <li class="nav-iten">
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a href="">Portfólio</a>
+                </li>
+                <li>
+                    <a href="">Pacotes</a>
+                </li>
+                <li>
+                    <a href="">Redes Sociais</a>
+                </li>
+                <li>
+                    <a href="">Reserva</a>
+                </li>
+            </ul>
+     
+    </nav>
+    <a href="#"><button>Contato</button></a>
+</header>
