f0fac756874d796f1481d6cb3dfd7eb815e5b4fa
jeffersonalisson1003-dotcom <jefferson.alisson1003@gmail.com>
2025-11-27 19:45:51 -0300

    Update faleconosco.php

--- projeto_base/faleconosco.php
+++ projeto_base/faleconosco.php
@@ -1,16 +1,17 @@
 <h3>Fale Conosco</h3>
 <form action="/action_page.php">
     <div class="mb-3 mt-3">
-        <label for="email" class="form-label">Email:</label>
+        <label for="email" class="form-label">Email:jessicaferreirafotografiaa@gmail.com</label>
         <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
     <div class="mb-3">
-        <label for="pwd" class="form-label">Nome:</label>
+        <label for="pwd" class="form-label">Nome:Jessica Ferreira</label>
         <input type="text" class="form-control" id="pwd" placeholder="Seu nome" name="pswd">
     </div>
     <div class="mb-3">
-        <label for="pwd" class="form-label">Mensagem:</label>
+        <label for="pwd" class="form-label">Mensagem:Envie uma mensagem indicando o ensaio desejado, informe a data e logo entraremos em contato para informar a disponibilidade.</label>
         <textarea type="text" class="form-control" id="pwd" placeholder="Mensagem" name="pswd"></textarea>
     </div>
     <button type="submit" class="btn btn-primary">Enviar</button>
+
 </form>
