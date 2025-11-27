2eb680de737276cf60cb7712dd8e36cf66b0175c
jeffersonalisson1003-dotcom <jefferson.alisson1003@gmail.com>
2025-11-27 19:20:18 -0300

    Add files via upload

--- rodape.php
+++ rodape.php
@@ -1,14 +1,34 @@
-<footer class="card">
-    
-    <address>
-        Rua do mataroudo - 593, Goiana, Pernambuco
-    </address>
-    
-    <small>
-        2025 &copy; Direitos Reservados
-    </small>
-    
-</footer>
+<?php
+$nome = "Jessica Ferreira";
+$endereco = "R. Profa. Alice Elisa de Melo, 198 - Mangabeira, João Pessoa - PB, 58057-400";
+$telefone = "(83) 99826-7756";
+$instagram_url = "https://www.instagram.com/jessicaferreirafotoart/";
+$ano_atual = date("Y"); // Obtém o ano atual automaticamente
+?>
 
-</body>
-</html>
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo $ano_atual; ?> **Direitos Reservados** | **<?php echo $nome; ?>*Jessica Ferreira Fotografia*</p>
+        
+        <hr>
+
+        <div class="footer-info">
+            <div class="info-section">
+                <h4>📍 Localização</h4>
+                <address>
+                    <?php echo $endereco; ?>
+                </address>
+            </div>
+            
+            <div class="info-section">
+                <h4>📞 Contato</h4>
+                <p>Telefone: **<a href="tel:<?php echo preg_replace('/[^\d]/', '', $telefone); ?>"><?php echo $telefone; ?></a>*(83) 99826-7756*</p>
+            </div>
+
+            <div class="info-section">
+                <h4>📸 Redes Sociais</h4>
+                <p>Instagram: **<a href="<?php echo $instagram_url; ?>" target="_blank">@jessicaferreirafotoart</a>**</p>
+            </div>
+        </div>
+    </div>
+</footer>
